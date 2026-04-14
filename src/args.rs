583bbc79b6eb4604a7e0d3f5f5f45b2a1efdae7b
Alexander Jackson <alex.jackson@starlingbank.com>
2026-04-14 12:41:05 +0100

    feat: add support for warmups

--- src/args.rs
+++ src/args.rs
@@ -3,6 +3,7 @@ use std::path::PathBuf;
 use color_eyre::eyre::Result;
 use pico_args::Arguments;
 
+#[derive(Clone, Debug)]
 pub struct Args {
     pub current: PathBuf,
     pub up: PathBuf,
@@ -10,6 +11,13 @@ pub struct Args {
     pub down: PathBuf,
     pub parameters: PathBuf,
     pub connection_details: PathBuf,
+    pub settings: Settings,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Settings {
+    pub warmups: usize,
+    pub runs: usize,
 }
 
 impl Args {
@@ -23,6 +31,10 @@ impl Args {
             down: args.value_from_str("--down")?,
             parameters: args.value_from_str("--parameters")?,
             connection_details: args.value_from_str("--connection-details")?,
+            settings: Settings {
+                warmups: args.opt_value_from_str("--warmups")?.unwrap_or(5),
+                runs: args.opt_value_from_str("--runs")?.unwrap_or(10),
+            },
         })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+use std::collections::HashMap;
 use std::path::Path;
 use std::str::FromStr;
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 use color_eyre::eyre::{eyre, Result};
 use serde::Deserialize;
-use sqlx::{types::Json, PgPool, Row};
+use sqlx::types::Json;
+use sqlx::{PgPool, Row};
 use uuid::Uuid;
 
 mod args;
 
-use crate::args::Args;
+use crate::args::{Args, Settings};
 
 #[derive(Deserialize)]
 struct QueryPlan {
@@ -35,34 +37,84 @@ async fn execute_as_owner(pool: &PgPool, query: &str) -> Result<()> {
     Ok(())
 }
 
-async fn benchmark_query(pool: &PgPool, query: &str, parameters: &[Uuid]) -> Result<()> {
+struct BenchmarkOutcome {
+    elapsed: Duration,
+    planning_time: f64,
+    execution_time: f64,
+}
+
+async fn run_query(pool: &PgPool, query: &str, parameter: Uuid) -> Result<BenchmarkOutcome> {
+    let mut tx = pool.begin().await?;
+    let now = Instant::now();
+
+    let result = sqlx::query(&query)
+        .bind(parameter)
+        .fetch_one(&mut *tx)
+        .await?;
+
+    let plans: Json<Vec<QueryPlan>> = result.try_get(0)?;
+    let plan = &plans[0];
+
+    let elapsed = now.elapsed();
+    tx.rollback().await?;
+
+    Ok(BenchmarkOutcome {
+        elapsed,
+        planning_time: plan.planning_time,
+        execution_time: plan.execution_time,
+    })
+}
+
+async fn benchmark_query(
+    pool: &PgPool,
+    query: &str,
+    parameters: &[Uuid],
+    settings: Settings,
+) -> Result<HashMap<Uuid, BenchmarkOutcome>> {
     let analyse_query = format!("EXPLAIN (ANALYZE, FORMAT JSON) {}", query);
+    let mut results = HashMap::new();
 
     for parameter in parameters {
-        let mut tx = pool.begin().await?;
-        let now = Instant::now();
+        println!("Benchmarking parameter {}...", parameter);
+        println!("Running {} warmup runs...", settings.warmups);
 
-        let result = sqlx::query(&analyse_query)
-            .bind(parameter)
-            .fetch_one(&mut *tx)
-            .await?;
+        for _ in 0..settings.warmups {
+            run_query(pool, &analyse_query, *parameter).await?;
+        }
 
-        let plans: Json<Vec<QueryPlan>> = result.try_get(0)?;
-        let plan = &plans[0];
+        let mut outcomes = Vec::new();
 
-        let elapsed = now.elapsed();
-        tx.rollback().await?;
+        println!("Running {} benchmark runs...", settings.runs);
 
-        println!(
-            "Parameter {}: Query took {} ms, planning time: {} ms, execution time: {} ms",
-            parameter,
-            elapsed.as_millis(),
-            plan.planning_time,
-            plan.execution_time
+        for _ in 0..settings.runs {
+            let outcome = run_query(pool, &analyse_query, *parameter).await?;
+            outcomes.push(outcome);
+        }
+
+        // remove the fastest and slowest runs to mitigate outliers
+        outcomes.sort_by_key(|o| o.elapsed);
+        outcomes.pop();
+        outcomes.remove(0);
+
+        // summarise the results by taking the average of the remaining runs
+        let average_elapsed =
+            outcomes.iter().map(|o| o.elapsed).sum::<Duration>() / (outcomes.len() as u32);
+        let average_planning_time =
+            outcomes.iter().map(|o| o.planning_time).sum::<f64>() / (outcomes.len() as f64);
+        let average_execution_time =
+            outcomes.iter().map(|o| o.execution_time).sum::<f64>() / (outcomes.len() as f64);
+
+        results.insert(
+            *parameter,
+            BenchmarkOutcome {
+                elapsed: average_elapsed,
+                planning_time: average_planning_time,
+                execution_time: average_execution_time,
+            },
         );
     }
 
-    Ok(())
+    Ok(results)
 }
 
 #[tokio::main]
@@ -86,11 +138,57 @@ async fn main() -> Result<()> {
     // open a connection to the database
     let pool = PgPool::connect(&connection_details).await?;
 
-    benchmark_query(&pool, &current_query, &parameters).await?;
+    let current = benchmark_query(&pool, &current_query, &parameters, args.settings).await?;
     execute_as_owner(&pool, &up_query).await?;
 
-    benchmark_query(&pool, &proposed_query, &parameters).await?;
+    let proposed = benchmark_query(&pool, &proposed_query, &parameters, args.settings).await?;
     execute_as_owner(&pool, &down_query).await?;
+
+    // compare the average execution and planning times based on percentage improvement or regression
+    let mut deltas = HashMap::new();
+
+    for parameter in &parameters {
+        let current_outcome = current.get(parameter).unwrap();
+        let proposed_outcome = proposed.get(parameter).unwrap();
+
+        let execution_time_change = (proposed_outcome.execution_time
+            - current_outcome.execution_time)
+            / current_outcome.execution_time
+            * 100.0;
+        let planning_time_change = (proposed_outcome.planning_time - current_outcome.planning_time)
+            / current_outcome.planning_time
+            * 100.0;
+
+        deltas.insert(*parameter, (execution_time_change, planning_time_change));
+
+        println!(
+            "Parameter {}: current execution time: {:.2}ms, current planning time: {:.2}ms, proposed execution time: {:.2}ms, proposed planning time: {:.2}ms, execution time change: {:.2}%, planning time change: {:.2}%",
+            parameter,
+            current_outcome.execution_time,
+            current_outcome.planning_time,
+            proposed_outcome.execution_time,
+            proposed_outcome.planning_time,
+            execution_time_change,
+            planning_time_change
+        );
+    }
+
+    // summarise the results by calculating the average percentage change across all parameters
+    let average_execution_time_change = deltas
+        .values()
+        .map(|(execution_time_change, _)| *execution_time_change)
+        .sum::<f64>()
+        / (deltas.len() as f64);
+    let average_planning_time_change = deltas
+        .values()
+        .map(|(_, planning_time_change)| *planning_time_change)
+        .sum::<f64>()
+        / (deltas.len() as f64);
+
+    println!(
+        "Average execution time change: {:.2}%, Average planning time change: {:.2}%",
+        average_execution_time_change, average_planning_time_change
+    );
 
     Ok(())
 }
